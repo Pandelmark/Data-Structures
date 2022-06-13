c5248e6e717c670fa59397f917e713daa905da6f
Pandelmark <73886448+Pandelmark@users.noreply.github.com>
2022-06-13 14:41:17 +0300

    Update myStack.c

--- myStack.c
+++ myStack.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include "myStack.h"
 
+//the function where the initialization of the stack takes place
 void init(struct STACK *st, int N){
 	st->mat = (int*)malloc(N*sizeof(int));
 	st->head = 0;
 	st->size = N;
 }
 
+//the function where elements are added (pushed) into the stack
 int push(struct STACK *st, int elem){
 	if(st->head == st->size)
 		return 0;
